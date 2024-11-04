0518b96689c109eae5c2c28f4ddd5552a06da8c9
AhXre <fredyesteban2004@gmail.com>
2024-11-04 14:07:27 -0500

    inicializacion de proyecto

---
+++ src/main/java/co/edu/unicauca/cuychair/gui/CuyChairGUI.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package co.edu.unicauca.cuychair.gui;
+
+import co.edu.unicauca.cuychair.gui.views.MenuView;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Frdy
+ */
+public class CuyChairGUI {
+
+    public static void main(String[] args) {
+        System.out.println("Starting application");
+        initApp();        
+    }    
+    
+    /**
+     * Inicializa la aplicación
+     */    
+    public static void initApp() {
+        JFrame menu = new MenuView();  
+        menu.setVisible(true);        
+    }
+    
+    /**
+     * Termina la aplicación
+     */
+    public static void terminate() {
+        System.out.println("Ending Application");
+    }
+}
